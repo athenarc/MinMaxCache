2f2bf73db99a0458f56353385017cbf5674392d4
billstam12 <billstam12@gmail.com>
2024-09-30 12:15:55 +0300

    Update to Java 17

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,6 +18,6 @@ do
        out="output_q=${q}_a=${a}_p=${p}_a_ratio=${aggFactor}_w=${w}_h=${h}"
         # out="output_scalability_q=${q}_a=${a}_p=${p}_a_ratio=${aggFactor}_r_ratio=${reductionRatio}"
         # out="m4-0.1"
-        java -jar target/experiments.jar -c timeQueries -seqCount 50 -measureChange 0 -type "$type" -mode "$mode" -measures 2 -timeCol epoch -valueCol value -idCol id -zoomFactor 2 -viewport "$w","$h" -runs 3 -out "$out" -minShift 0.1 -maxShift 0.5 -schema more -table "$table" -timeFormat "yyyy-MM-dd[ HH:mm:ss.SSS]" -a "$a" -q "$q" -p "$p" -agg "$aggFactor" -queries queries.txt
+        java -jar target/experiments.jar -c timeQueries -seqCount 50 -measureChange 0 -type "$type" -mode "$mode" -measures 2 -timeCol epoch -valueCol value -idCol id -zoomFactor 2 -viewport "$w","$h" -runs 3 -out "$out" -minShift 0.1 -maxShift 0.5 -schema more -table "$table" -timeFormat "yyyy-MM-dd[ HH:mm:ss.SSS]" -a "$a" -q "$q" -p "$p" -agg "$aggFactor"
     done
 done
